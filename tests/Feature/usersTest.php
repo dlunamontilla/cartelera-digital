008eab7f4f459a35df936183676f3a5c43d8ac56
David E Luna M <davidlunamontilla@gmail.com>
2021-03-18 02:19:36 -0400

    Preparacion del entorno de trabajo

---
+++ tests/Feature/usersTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class usersTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        // $this->get("/usuarios/")->assertStatus(200)->assertSee("Ciencia de datos");
+    }
+}
